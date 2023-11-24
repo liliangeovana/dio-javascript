06bb4ea72fcf0e02c85eec4353e5774de5996c29
Lílian Saraiva <lili.ggsb@gmail.com>
2023-11-24 16:42:16 -0400

    atualizando ultima atividade

--- modulo2/exercicio3.js
+++ modulo2/exercicio3.js
@@ -11,25 +11,22 @@ function AcrescentarJuros(preco, acrescimo){
 (function (){
    var formaDePagamento = 2;
    var valorPagamento = 100;
-   var pagamento;
 
    if (formaDePagamento=== 1){
       formaDePagamento = "Débito";
-      pagamento = AplicarDesconto(valorPagamento, 10);
+      console.log(AplicarDesconto(valorPagamento, 10));
 
   }else if (formaDePagamento === 2){
       formaDePagamento= "Dinheiro ou Pix";
-      pagamento = AplicarDesconto(valorPagamento, 15);
-      
+      console.log(AplicarDesconto(valorPagamento, 15));   
   }else if (formaDePagamento === 3){
       formaDePagamento = "Parcelado 2x";
-      valorPagamento;
+      console.log(valorPagamento)
   }else{
       formaDePagamento ="Parcelado em mais de 2x"
-      pagamento = AcrescentarJuros(valorPagamento, 10)
+      console.log(AcrescentarJuros(valorPagamento, 10));
       
   }
 
    console.log(formaDePagamento)
-   console.log(pagamento)
 })();
