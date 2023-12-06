8c098965946af782a5cc10e399e2becac182ad54
Lílian Saraiva <lili.ggsb@gmail.com>
2023-12-06 15:46:51 -0400

    finalizando modulo 4

---
+++ modulo4/1.aula-for-string.js
@@ -0,0 +1,5 @@
+const nome = "Fulano Sicrano";
+
+for (let i = 0; i < nome.length; i++) {
+   console.log(nome[i]);   
+}
---
+++ modulo4/exercicio1.js
@@ -0,0 +1,7 @@
+// Definido um número, exiba a tabuada do mesmo
+
+const valor = 2;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(valor, " x ", i, " = ", valor*i)
+}
