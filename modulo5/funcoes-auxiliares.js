cd8e8ff8ea5c18e6ef6c3eff8aa0642e680cda2f
Lilian Geovana Garcia Saraiva Botelho <lilian.botelho@rr.sebrae.com.br>
2023-12-07 11:16:41 -0300

    finalizando modulo 5 e o primeiro bloco de cursos

--- modulo5/funcoes-auxiliares.js
+++ modulo5/funcoes-auxiliares.js
@@ -1,5 +1,10 @@
+const conjunto = [5, 50, 10, 98, 23];
+let i = 0;
+
 function gets() {
-    return 10;
+    const valor = conjunto[i];
+    i++
+    return valor;
 }
 
 function print(texto) {
--- modulo5/main.js
+++ modulo5/main.js
@@ -1,2 +1,14 @@
-const funcoes = require('./funcoes-auxiliares');
+const {gets, print} = require('./funcoes-auxiliares');
 
+const numerosSorteados = [];
+let maiorValor = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numero = gets();
+    if (numero > maiorValor){
+        maiorValor = numero;
+    }
+    numerosSorteados.push(numero);
+}
+
+print(maiorValor);
