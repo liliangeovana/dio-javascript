1cb642b451a78272432f8983ddc42a688b2c93a9
Lílian Saraiva <lili.ggsb@gmail.com>
2024-01-18 16:28:04 -0400

    update javascript

--- bloco2/pokedex/javascript/main.js
+++ bloco2/pokedex/javascript/main.js
@@ -1,9 +1,8 @@
-const offset = 0;
-    const limit = 10;
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-    function convertPokemonToList(pokemon) {
-        return `<li class="pokemon">
+function convertPokemonToList(pokemon) {
+        return `
+        
+        <li class="pokemon">
                     <span class="number">#001</span>
                         <span class="name">${pokemon.name}</span>
                             <div class="details">
@@ -14,18 +13,16 @@ const offset = 0;
                                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}"/>
                             </div>           
                     </li>`    
-    }
+}
 
-    console.log(document.getElementById('pokemonsList'));
+const pokemonList = document.getElementById('pokemonList');
+      
+PokeAPI.getPokemons().then((pokemonCount = []) => {
 
-    //fetch(url)
-        //.then((response) =>  response.json())
-       // .then((jsonBody) => jsonBody.results)
-       // .then((pokemonList) => {
-           // for (let i = 0; i < pokemonList.length; i++) {
-            //    const pokemon = pokemonList[i];
-           //     console.log(convertPokemonToList(pokemon));
+    const newList = pokemonCount.map((pokemon) => convertPokemonToList(pokemon));
 
-          //  }
-      //  })
-       // .catch((error) =>  console.error(error));
+    const newHTML = newList.join('');
+
+    pokemonList.innerHTML += newHTML
+
+})
